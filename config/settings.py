e1d6b6d70842bd0010375106b3499f9fa288c370
brunomcduarte96 <bruno.mcduarte96@gmail.com>
2025-02-20 01:12:05 -0300

    2

--- config/settings.py
+++ config/settings.py
@@ -1,35 +1,30 @@
-import os
-from dotenv import load_dotenv
 import streamlit as st
-
-# Carrega variáveis de ambiente
-load_dotenv()
 
 # Configurações do Supabase
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 # Configurações do Google
-GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
+GOOGLE_CREDENTIALS = st.secrets["GOOGLE_CREDENTIALS"]
 SHEETS_SCOPE = ['https://www.googleapis.com/auth/spreadsheets']
 DRIVE_SCOPE = ['https://www.googleapis.com/auth/drive']
 DOCS_SCOPE = ['https://www.googleapis.com/auth/documents']
 
 # IDs das planilhas do Google Sheets
-SHEET_ID_1 = os.getenv("SHEET_ID_1")
-SHEET_ID_2 = os.getenv("SHEET_ID_2")
+SHEET_ID_1 = st.secrets["SHEET_ID_1"]
+SHEET_ID_2 = st.secrets["SHEET_ID_2"]
 
 # ID da pasta raiz no Google Drive
-ROOT_FOLDER_ID = os.getenv("ROOT_FOLDER_ID")
+ROOT_FOLDER_ID = st.secrets["ROOT_FOLDER_ID"]
 
 # Configurações do OpenAI
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 # URLs do Menu
 URLS = {
-    "CLIENTES_SMARTLEGAL": os.getenv("URL_CLIENTES"),
-    "PROCESSOS_ANDAMENTO": os.getenv("URL_PROCESSOS"),
-    "LEADS_ADS": os.getenv("URL_LEADS"),
-    "CRM_RD": os.getenv("URL_CRM"),
-    "DRIVE_GMAIL": os.getenv("URL_DRIVE")
+    "CLIENTES_SMARTLEGAL": st.secrets["URL_CLIENTES"],
+    "PROCESSOS_ANDAMENTO": st.secrets["URL_PROCESSOS"],
+    "LEADS_ADS": st.secrets["URL_LEADS"],
+    "CRM_RD": st.secrets["URL_CRM"],
+    "DRIVE_GMAIL": st.secrets["URL_DRIVE"]
 } 
